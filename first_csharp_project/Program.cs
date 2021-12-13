df1386677e57b4aa406b22fa5cf26e3217154d96
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2021-12-13 17:55:11 +0500

    Add project files.

---
+++ first_csharp_project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_csharp_project
+{
+    class Program
+    {static void m()
+        {
+            Console.WriteLine("im function");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            m();
+            Console.ReadLine();
+        }
+    }
+}
